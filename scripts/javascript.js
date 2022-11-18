7cb81c1f0614b65d7f014d9b95fc5ddf919b12b9
Robert Schwindler <robertmichaelschwindler@gmail.com>
2022-11-18 07:30:05 -0500

    Update v2.1

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,1 +1,21 @@
 'use strict'
+submitBtn = document.getElementById('submitBtn');
+email = document.getElementById('email');
+
+submitBtn.addEventListener('click', function(){
+    validateEmail(email.value)
+});
+
+
+function validateEmail(inputText)
+{
+    let mailFormat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(inputText.match(mailFormat))
+    {
+        alert("This is a valid email address");
+        return true;
+    }else {
+        alert("Invalid email address!");
+        return false;
+      }
+}
