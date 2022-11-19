ba07d7fa56f3cdf0bfcbf88d0916947ddc123237
Robert Schwindler <robertmichaelschwindler@gmail.com>
2022-11-19 09:09:04 -0500

    Update v2.3
    Added Email Validation and form Javascript.
    Functionality is done

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,21 +1,36 @@
 'use strict'
-submitBtn = document.getElementById('submitBtn');
-email = document.getElementById('email');
-
-submitBtn.addEventListener('click', function(){
-    validateEmail(email.value)
-});
+const submitBtn = document.getElementById('submitBtn');
+const email = document.getElementById('email');
+const errMsg = document.getElementById('errMsg');
+const fname = document.getElementById('fName');
+const lname = document.getElementById('lName');
+const phone = document.getElementById('phone');
+const comments = document.getElementById('comments');
+const thanks = document.getElementById('thanks');
 
 
 function validateEmail(inputText)
 {
     let mailFormat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if(inputText.match(mailFormat))
-    {
-        alert("This is a valid email address");
+    if(inputText.match(mailFormat)){
+        //emailError.innerHTML = 'Thank you!';
+        email.value = '';
+        fname.value = '';
+        lname.value = '';
+        phone.value = '';
+        comments.value = '';
+        thanks.innerHTML = 'Thank you'
         return true;
-    }else {
-        alert("Invalid email address!");
+        
+	  }else {
+        email.value = '';
+        email.placeholder = 'This is not a valid email'; 
+        email.focus();
         return false;
       }
 }
+
+submitBtn.addEventListener('click', function(){
+    let passEmail = email.value;
+    validateEmail(passEmail);
+});
